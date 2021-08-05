70ce01a1e736a96a81489f7dd05483f6908c3d6b
raushanverma9523 <88338770+raushanverma9523@users.noreply.github.com>
2021-08-05 14:56:31 +0530

    Add files via upload
    i use html , css , bootstrap, php for coding and decorate for it.

---
+++ simple_form_formate.php
@@ -0,0 +1,81 @@
+<html>
+<head>
+<title color="green"> my first web page </title>
+</head>
+<body bgcolor="red" align="center">
+<h1> welcome to my fisrt web page </h1>
+<marquee direction="right" bgcolor="yellow" behavior="alternate"><h2>programming classes</h2></marquee>
+<marquee direction="right" bgcolor="white" behavior="slide"><h2>programming classes</h2></marquee>
+<br>
+<img src="copy1.jpg" width=50% alt="myimg">
+<br>
+<p><b><h2>programming<i>&nbsp classes</i><strike><mark>&nbsp patna</mark></strike></h2></b></p>
+<br>
+<p align="right"><h1>water formula=H<SUB>2</SUB>O</H1></p>
+<br>
+<ol>
+ <li>tea</li>
+ <li>cofee</li>
+ <li>milk</li>
+ <li>lassi</li>
+</ol>
+<br>
+<a href="https://www.w3schools.com" target="_blank">This is a link to open html </a>
+<br>
+<mark>(a+b)</mark><sup>2</sup>=a<sup>2</sup>+b<sup>2</sup>+<blink>2ab</blink>
+<h2>to more update click on the link</h2><a href="new1.php" target="_blank">update</a>
+<BR>
+<p><h1><b><mark>ssc online form apply</mark></b><h1></p>
+<input type="text">
+<br>
+<br>
+<input type="text" placeholder="name">
+<br><br>
+<input type="text" placeholder="father's name">
+<br><br>
+<input type="number" name="age" placeholder="age" min="17" max="50">
+<br><br>
+<input type="password" name="pass" placeholder="set password">
+<br><br>
+<input type="email" name="email" placeholder="email id">
+<br><br>
+<input type="date">
+<br><br>
+<lable><p> GENDER</p></lable>
+
+<input type="radio" name="gender">male
+<input type="radio" name="gender">female
+<br>
+<p><h2><b>category</b></h2>
+<input type="radio" name="cate">gen 
+<input type="radio" name="cate">bc1
+<input type="radio" name="cate">bc2
+<input type="radio" name="cate">obc
+<input type="radio" name="cate">ews
+<input type="radio" name="cate">sc/st </p>
+<br>
+<br>
+<p><h2>post selection<h2>
+<input type="checkbox" name="course">assistant a/c officer
+<input type="checkbox" name="course">ass section officer 
+<input type="checkbox" name="course">ssc cgl
+<input type="checkbox" name="course">ssc gd 
+<input type="checkbox" name="course">stenographer
+<input type="checkbox" name="course">ssc si </p>
+<br><br>
+<select name="course">
+    <option value="c1">c-prog</option>
+	<option value="c1">java</option>
+	<option value="c1">c#</option>
+	<option value="c1">.net</option>
+	<option value="c1">cpp</option>
+</select>
+<br>
+<br>
+<p><h2>choose image</h2><input type="file"></p>
+<br>
+<p><h2>choose signature</h2><input type="file"></p>
+<br><br>
+<input type="submit">
+</body>
+</html>
